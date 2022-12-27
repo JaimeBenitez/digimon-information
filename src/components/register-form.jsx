5532c3f840803f1d2a117e54ee1c5145471007f3
jaime <jbenaci736@g.educaaand.es>
2022-12-27 20:07:26 +0100

    Pagina registro con validaciones terminada

--- src/components/register-form.jsx
+++ src/components/register-form.jsx
@@ -1,19 +1,35 @@
 import React,{ useState } from "react";
 import BackArrow from "./back-arrow";
+import useScript from "../hooks/useScript";
 
 //Podemos declarar variables fuera de la función componente y usarlos dentro del mismo
+/*El email puede tener cualquier caracter en cualquier cantidad siempre y cuando no sea una @, espacio o tabulación en la primera parte
+Luego tendrá una @, luego otro set igual que en la primera parte, un punto y finalmente otro set del mismo tipo*/
 const emailRegexp = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
+/* La contraseña debera tener al menos una mayuscula, minuscula y digito,permite caracteres especiales y contara de entre 8 y 16 caracteres*/ 
+const passwordRegexp = new RegExp(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/);
+//El usuario admitirá cualquier caracter pero tendrá entre 3 y 20 caracteres
+const usernameRegexp = new RegExp(/^[\S]{3,20}$/);
+
 
 function RegisterForm(){
     
     //validación de formulario
     const [values,setValues] = useState({
-        email: "",
-        hasError: false,
+        username: "",
+        password: "",
+        email: "", 
+        date: "",           
+        usernameError: "",   
+        passwordError: false,
+        emailError: false,
+        dateError: false,           
     });
 
     function handleSubmit(e){
+        
         e.preventDefault();
+        alert("All right, welcome to the digiworld!")
     }
 
     function handleChange(e){
@@ -29,38 +45,101 @@ function RegisterForm(){
         //Por ultimo sincronizamos el nuevo estado
         setValues(newValues);
     }
-    
-    function handleError(){
+    function handleUsernameError(){
         //Testea si pasa la regex y de no ser asi lanza el error. Esto solo ocurrirá cuando se quite el foco del elemento, previninendo
         //que el usuario vea el mensaje de error mientras rellena el campo
-        const hasError = !emailRegexp.test(values.email);
-        setValues((prevState) => ({ ...prevState, hasError}))
+        const usernameError = !usernameRegexp.test(values.username);
+        setValues((prevState) => ({ ...prevState, usernameError}));
     }
+
+    function handleEmailError(){
+        
+        const emailError = !emailRegexp.test(values.email);
+        setValues((prevState) => ({ ...prevState, emailError}));
+    }
+
+    function handlePasswordError(){
+        
+        const passwordError = !passwordRegexp.test(values.password);
+        setValues((prevState) => ({ ...prevState, passwordError}));
+    }
+
+    function handleDateError(){
+        //Comprobación de la fecha, aunque el tipo date ya comprueba todo esto es mejor revalidar por si el usuario modifica el HTML
+        //Ojo que el tipo date le da la vuelta a la fecha, empieza por años
+        let testDays = true;
+        const day = parseInt(values.date.split('-')[2]);
+        const month = parseInt(values.date.split('-')[1]);
+        const year = parseInt(values.date.split('-')[0]);
+        
+        const monthDays = new Date(year, month,0).getDate()
+        console.log (monthDays)
+        if (day > monthDays) {
+            testDays =  false
+        }        
+        //Comprobacion del año actual
+        let testYear = true;
+        const currentYear = new Date().getFullYear();
+
+        if (year > currentYear){
+            testYear = false
+        }
+
+        const dateError = !testDays || !testYear;
+        
+        console.log(dateError)
+        setValues((prevState) => ({ ...prevState, dateError}));
+
+    }
+        //Seteamos el dia maximo al dia de hoy
+       const today = new Date().toISOString().split("T")[0];
+       
     //Fin de validación
+    
     
 
     //OnBlur se activa al retirar el foco de un elemento
     return (
         <form onSubmit={handleSubmit} id="register__form">   
             <fieldset id="register__fieldset"> 
-                <h2 className="register__subtitle">Register Data</h2>            
+                <h2 className="register__subtitle">Register Data</h2> 
+                
+                <label  htmlFor="username" className="formLabel" id="firstLabel">Username</label>           
                 <input type="text" name="username" id="register__username" className="register__input" 
-                placeholder="Username" />
+                placeholder="Username" value={values.username} onChange={handleChange} onBlur={handleUsernameError} aria-errormessage="usernameError"
+                aria-invalid={values.usernameError} required/>
+                <span className="error" id="usernameError" aria-live="assertive" style={{visibility: values.usernameError ? "visible" : "hidden"}}>
+                    Username invalid. Please enter a valid username</span>
+
+                <label  htmlFor="password" className="formLabel">Password</label>   
                 <input type="password" name="password" id="register__password" className="register__input" 
-                placeholder="Password" />
+                placeholder="Password" value={values.password} onChange={handleChange} onBlur={handlePasswordError} aria-errormessage="passwordError"
+                aria-invalid={values.passwordError} required/>
+                <span className="error" id="passwordError" aria-live="assertive" style={{visibility: values.passwordError ? "visible" : "hidden"}}>
+                    Password invalid. Please enter a valid password</span>
+
+                <label  htmlFor="email" className="formLabel">Email</label>
                 <input type="email" name="email" id="register__email" className="register__input" 
-                placeholder="email" value={values.email} onChange={handleChange} onBlur={handleError} aria-errormessage="emailError"
-                aria-invalid={values.hasError} /> 
+                placeholder="email" value={values.email} onChange={handleChange} onBlur={handleEmailError} aria-errormessage="emailError"
+                aria-invalid={values.emailError} required/> 
                 {/* El span solo sera visible si se triggerea el error */}
-                <span id="emailError" aria-live="assertive" style={{visibility: values.hasError ? "visible" : "hidden"}}>
+                <span className="error" id="emailError" aria-live="assertive" style={{visibility: values.emailError ? "visible" : "hidden"}}>
                     Email invalid. Please enter a valid e-mail</span>
-                <input type="date" name="date" id="register__date" className="register__input"  />                            
+
+                <label  htmlFor="date" className="formLabel">Borndate</label>
+                <input type="date" name="date" id="register__date" className="register__input"  value={values.date} onChange={handleChange}
+                 onBlur={handleDateError} aria-errormessage="dateError" aria-invalid={values.dateError} max={today} required/>  
+                  <span className="error" id="dateError" aria-live="assertive" style={{visibility: values.dateError ? "visible" : "hidden"}}>
+                    Borndate invalid. Please enter a valid borndate</span> 
+
                 <button type="submit" name="submit" id="register__submit" className="buttons">Submit</button>
                 <a href="login.html" id="register__toLogin">Do you have an account? Go to login</a>
                 <img src={require("../assets/img/Agumon_registro-sf.png")} alt="agumon" id="register__img" />
                 <BackArrow />                        
-            </fieldset>                
+            </fieldset>    
+                       
         </form>
+        
     )
 }
 export default RegisterForm;
